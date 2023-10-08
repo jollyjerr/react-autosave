e32ac47e42b0d4ca3df0b1591cf3bad270115f23
Jeremiah Tabb <jollyjerr@gmail.com>
2023-10-07 19:52:51 -0600

    fix!: esm typescript support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ outDir: 'dist', rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'react-autosave',
+      formats: ['es'],
     },
     rollupOptions: {
       external: ['react'],
